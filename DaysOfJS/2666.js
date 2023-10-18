5c5ce4e7b09abf30fdea34b4f472e46b9f7a97f7
Henri Schmeing <h.schmeing@schenckprocess.com>
2023-10-18 09:18:06 +0200

    Two more Days :)

---
+++ DaysOfJS/2666.js
@@ -0,0 +1,18 @@
+﻿// 2666. Allow One Function Call
+
+var once = function(fn) {
+
+    let called = false;
+    return function(...args){
+        if(called === true) return undefined
+        called = true; 
+        return fn(...args)
+    }
+};
+
+let fn = (a,b,c) => (a + b + c)
+let onceFn = once(fn)
+
+console.log(onceFn(1,2,3)); // 6
+console.log(onceFn(2,3,6)); // returns undefined without calling fn
+
---
+++ DaysOfJS/2703.js
@@ -0,0 +1,7 @@
+﻿// 2703. Return Length of Arguments Passed
+
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+console.log(argumentsLength(1, 2, 3)); // 3
