d020f64f375629bc74c30d8ba691d3698f4618f5
Henri Schmeing <h.schmeing@schenckprocess.com>
2023-10-19 08:23:56 +0200

    A Good Morning.

---
+++ DaysOfJS/2715.js
@@ -0,0 +1,15 @@
+﻿// 2715. Timeout Cancellation
+var cancellable = function(fn, args, t) {
+    let isCancelled = false;
+    
+    const timer = setTimeout(() => {
+        if(!isCancelled) {
+            fn(...args);
+        }
+    }, t);
+    
+    return function cancelFn(){
+        isCancelled = true;
+        clearTimeout(timer);
+    }
+};
---
+++ DaysOfJS/2725.js
@@ -0,0 +1,17 @@
+﻿// 2725. Interval Cancellation
+
+var cancellable = function(fn, args, t) {
+    let isCancelled = false;
+    fn(...args);
+    
+    const timer = setInterval(() => {
+        if(!isCancelled) {
+            fn(...args);
+        }
+    }, t);
+    
+    return function cancelFn(){
+        isCancelled = true;
+        clearInterval(timer);
+    }
+};
