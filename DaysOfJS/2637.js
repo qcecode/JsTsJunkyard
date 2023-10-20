af98727dfb5fc7a181c6103ce79cf05e028da6fb
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-20 22:39:11 +0200

    one more day

---
+++ DaysOfJS/2637.js
@@ -0,0 +1,20 @@
+﻿// 2637. Promise Time Limit
+
+function timeLimit(fn, t) {
+    return async (...args) => {
+        let timeout;
+
+        const timerPromise = new Promise((_, reject) => {
+            timeout = setTimeout(() => reject('Time Limit Exceeded'), t);
+        });
+
+        try {
+            const result = await Promise.race([fn(...args), timerPromise]);
+            clearTimeout(timeout);
+            return result;
+        } catch (err) {
+            clearTimeout(timeout);
+            throw err;
+        }
+    };
+}
