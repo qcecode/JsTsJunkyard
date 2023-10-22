a688f2643c5ae833e7791b3e94cc00666d0cd752
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-22 10:07:29 +0200

    one More Day

---
+++ DaysOfJS/2622.js
@@ -0,0 +1,44 @@
+﻿// 2622. Cache With Time Limit
+
+class TimeLimitedCache {
+    constructor() {
+        this.data = new Map();
+    }
+
+    set(key, value, duration) {
+        const currentTime = Date.now();
+
+        // If key exists and is not expired
+        if (this.data.has(key) && this.data.get(key).expirationTime > currentTime) {
+            this.data.set(key, { value: value, expirationTime: currentTime + duration });
+            return true;
+        } else {
+            this.data.set(key, { value: value, expirationTime: currentTime + duration });
+            return false;
+        }
+    }
+
+    get(key) {
+        const currentTime = Date.now();
+
+        // If key exists and is not expired
+        if (this.data.has(key) && this.data.get(key).expirationTime > currentTime) {
+            return this.data.get(key).value;
+        } else {
+            return -1;
+        }
+    }
+
+    count() {
+        const currentTime = Date.now();
+
+        // Filter out the expired keys
+        let validCount = 0;
+        for (let entry of this.data.entries()) {
+            if (entry[1].expirationTime > currentTime) {
+                validCount++;
+            }
+        }
+        return validCount;
+    }
+}
