bbe2e980828efab9c964d3d5fc7c4e2ff90c86fa
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-22 10:21:19 +0200

    2627. Debounce

---
+++ DaysOfJS/2627.js
@@ -0,0 +1,16 @@
+﻿// 2627. Debounce
+
+var debounce = function(fn, t) {
+    let timeoutId = null;
+    
+    return function(...args) {
+        if (timeoutId !== null) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+            timeoutId = null;
+        }, t);
+    }
+};
