2a9d9899e201c950037cada660dd7ee94ff78c5c
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-26 13:03:55 +0200

    2722. Join Two Arrays by ID

---
+++ DaysOfJS/2722.js
@@ -0,0 +1,30 @@
+﻿// 2722. Join Two Arrays by ID
+
+var join = function (arr1, arr2){
+
+    const result = {};
+
+    // 1. initialization
+    arr1.forEach(item => {
+        result[item.id] = item;
+    });
+    // 2. joining
+    arr2.forEach(item => {
+        if (result[item.id]) {
+            Object.keys(item).forEach(key => {
+                result[item.id][key] = item[key];
+            });
+        } else {
+            result[item.id] = item;
+        }
+    });
+
+    return Object.values(result);
+
+}
+
+
+
+var testArr1 = [{"id":1,"b":{"b": 94},"v":[4,3],"y":48}]
+var testArr2 = [{"id":1,"b":{"c": 84},"v":[1,3]}]
+console.log(join(testArr1,testArr2));
