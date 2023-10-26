3b703f26c05f754b25b774f0d478df2843652608
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-26 07:10:57 +0200

    I SUCK AT JS :)

---
+++ DaysOfJS/2724.js
@@ -0,0 +1,12 @@
+﻿// 2724. Sort By
+
+var sortBy = function(arr, fn) {
+
+    return arr.sort((a,b) => fn(a) - fn(b));
+    
+};
+
+var fn = (x) => x
+var arr = [5,4,1,2,3];
+
+console.log(sortBy(arr,fn));
