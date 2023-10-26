b8386651480512b3b8d6d39cfc78ff30b6b86932
Henri Schmeing <h.schmeing@schenckprocess.com>
2023-10-26 06:43:16 +0200

    2631. Group By

---
+++ DaysOfJS/2631.js
@@ -0,0 +1,16 @@
+// 2631. Group By
+
+Array.prototype.groupBy = function(fn) {
+
+    return this.reduce((grouped, item) => {
+        const key = fn(item);
+        
+        if(!grouped[key]) {
+            grouped[key] = [];
+        }
+        
+        grouped[key].push(item);
+        return grouped;
+    }, {})
+};
+
