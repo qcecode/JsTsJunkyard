7baa1082970332cc3c4f4114d95e46345d260414
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-27 09:22:43 +0200

    2625. Flatten Deeply Nested Array

---
+++ DaysOfJS/2625.js
@@ -0,0 +1,18 @@
+﻿// 2625. Flatten Deeply Nested Array
+
+var flat = function (arr, n) {
+    let returnArr = [];
+
+    for (const arrElement of arr) {
+        if (Array.isArray(arrElement) && n > 0) {
+            returnArr.push(...flat(arrElement, n-1));
+        } else {
+            returnArr.push(arrElement);
+        }
+    }
+    
+    return returnArr;
+};
+
+var testArr = [1,2,3,[4,5,6],[7,8,[9,10,11],12],[13,14,15]];
+console.log(flat(testArr, 1));
