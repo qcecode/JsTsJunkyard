641c8e9f88368eb4569c7ee76c23a988fec023d9
Henri Schmeing <henri.schmeing@gmail.com>
2023-10-28 09:11:12 +0200

    2705. Compact Object. Fun with recursion :)

---
+++ DaysOfJS/2705.js
@@ -0,0 +1,36 @@
+﻿// 2705. Compact Object
+
+var compactObject = function(obj) {
+    // Base case
+    if(typeof obj !== 'object' || obj === null){
+        return obj
+    }
+
+    // Case for arrays
+    if(Array.isArray(obj)){
+        const compactArr = []
+        for(let i = 0; i < obj.length; i++){
+            let val = compactObject(obj[i]);
+            if(val){
+                compactArr.push(val)
+            }
+        }
+
+        return compactArr
+    }
+
+    // Case for objects
+    const compactObj = {};
+
+    for(let key in obj){
+        const val = compactObject(obj[key]);
+        if(val){
+            compactObj[key] = val
+        }
+    }
+
+    return compactObj
+};
+
+var testObj = [null, 0, false, 1];
+console.log(compactObject(testObj));
