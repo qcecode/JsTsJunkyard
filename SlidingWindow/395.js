dfde1806b271108d71660e620c88ab0ee74d9da0
Henri Schmeing <henri.schmeing@gmail.com>
2023-11-09 10:41:59 +0100

    395. Longest Substring with At Least K Repeating Characters

---
+++ SlidingWindow/395.js
@@ -0,0 +1,37 @@
+﻿// 395. Longest Substring with At Least K Repeating Characters
+
+// stupid Solution
+var longestSubstring = function(s, k) {
+    let maxLength = 0;
+
+    let lastChar;
+    let charJ
+    
+    for (let j = 0; j < s.length; j++) {
+        let map = new Map;
+        charJ = s.charAt(j);
+        if(lastChar === charJ){
+            continue
+        }
+        for (let i = j; i < s.length; i++) {
+            let char = s.charAt(i);
+            if (map.has(char)) {
+                map.set(char, map.get(char) + 1)
+            } else {
+                map.set(char, 1);
+            }
+            // alle Values Checken
+            let arr = [...map.values()]
+            if (!arr.some(element => element < k)) {
+                maxLength = Math.max(i - j + 1, maxLength);
+            }
+        }
+        lastChar = charJ;
+    }
+    return maxLength;
+};
+
+let testS = "caaaacac";
+let testK = 4;
+
+console.log(longestSubstring(testS,testK));
