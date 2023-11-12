3ee54cd9130919da74e838c16e1528be9d8b2482
Henri Schmeing <henri.schmeing@gmail.com>
2023-11-12 09:34:23 +0100

    643. Maximum Average Subarray I

---
+++ SlidingWindow/643.js
@@ -0,0 +1,42 @@
+﻿// 643. Maximum Average Subarray I
+
+// first try :)
+var findMaxAverage2 = function(nums, k) {
+    // setup
+    let sum = 0;
+    for(let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    let maxAvg = sum / k;
+    
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i];
+        sum -= nums[i-k];
+        let avg = sum / k;
+        if(avg > maxAvg){
+            maxAvg = avg;
+        }
+    }
+    
+    return maxAvg
+};
+
+var findMaxAverage = function(nums, k) {
+    let sum = 0;
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+
+    let maxAvg = sum / k;
+
+    for (let i = k; i < nums.length; i++) {
+        sum += nums[i] - nums[i - k];
+        maxAvg = Math.max(maxAvg, sum / k);
+    }
+
+    return maxAvg;
+};
+
+let testNums =  [1,12,-5,-6,50,3]
+let testK = 4
+console.log(findMaxAverage(testNums, testK));
